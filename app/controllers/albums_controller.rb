5fe1088430a4d8b15d4f86b05aa9be670c4015db
lifeprolific <thegregoman@gmail.com>
2018-08-22 15:45:13 -0400

    added errors and edit

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -4,12 +4,25 @@ class AlbumsController < ApplicationController
   end
 
   def new
+    @album = Album.new
   end
 
   def create
     @album = Album.new(params.require(:album).permit(:title, :artist_name, :year_released))
-    @album.save
-    redirect_to @album
+    if @album.save
+      redirect_to @album
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @album = Album.find(params[:id])
+    if @album.update(params.require(:album).permit(:title, :artist_name, :year_released))
+      redirect_to @album
+    else
+      render 'edit'
+    end
   end
 
   def show
@@ -17,5 +30,6 @@ class AlbumsController < ApplicationController
   end
 
   def edit
+    @album = Album.find(params[:id])
   end
 end
