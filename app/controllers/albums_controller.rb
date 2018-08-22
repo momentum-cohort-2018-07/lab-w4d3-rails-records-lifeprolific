b3e4976548e79a68eb399ffa97f8a09b6052ab81
lifeprolific <thegregoman@gmail.com>
2018-08-22 14:57:23 -0400

    added index page and links

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,11 +1,19 @@
 class AlbumsController < ApplicationController
   def index
+    @albums = Album.all
   end
 
   def new
   end
 
+  def create
+    @album = Album.new(params.require(:album).permit(:title, :artist_name, :year_released))
+    @album.save
+    redirect_to @album
+  end
+
   def show
+    @album = Album.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'albums/index'
-  get 'albums/new'
-  get 'albums/show'
-  get 'albums/edit'
+
+  resources :albums
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
